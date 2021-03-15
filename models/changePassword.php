71f35add4c973ccc6d240f2548c430d2459908b2
Olumide fowowe <volumide42@gmail.com>
2021-03-15 17:33:42 +0100

    store responsee in a variable

--- models/changePassword.php
+++ models/changePassword.php
@@ -2,6 +2,7 @@
 	require_once '../connections/connection.php';
 	$dbConnection = (new Conn())->connect();
 	
+	$message;
 	$oldPassword = "";
 	$newPassword = "";
 	$confirmPassword = "";
@@ -9,7 +10,6 @@
 		echo "unable to perfom the operation";
 		return;
 	}
-
 	$query = "SELECT `password` FROM `users` WHERE id = $id LIMIT 1";
 	$result = $dbConnection->query($query);
 
@@ -17,19 +17,16 @@
 	$password = $result;
 
 	if ($password !== $oldPassword){
-		echo "Your password does not match";
+		$message = "Your password does not match";
 		return;
 	}
 
 	$id = $_GET['id'];
 	if ($newPassword !== $confirmPassword) {
-		echo "Password does not match";
+		$message = "Password does not match";
 		return;
 	}
 
-	$query = "UPDATE `movies` SET 
-		`password` = `$newPassword`,
-	WHERE id = `$id`";
-	// return result by id based on query
-	return ($this->dbConnection->query($query)) ? "Record Updated successfully": "No result found";
+	$query = "UPDATE `movies` SET `password` = `$newPassword` WHERE id = `$id`";
+	$message =  ($this->dbConnection->query($query)) ? "Record Updated successfully": "No result found";
 ?>
