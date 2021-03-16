6f30a46e54118b527b64b4dff8e25db1f4c093c2
Olumide fowowe <volumide42@gmail.com>
2021-03-16 10:12:12 +0100

    finish signin form flow

--- models/signin.php
+++ models/signin.php
@@ -2,24 +2,22 @@
 	require_once '../connections/connection.php';
 	$dbConnection = (new Conn())->connect();
 	
-	$email = "";
-	$password = "";
+	$email = $_POST['email'];
+	$password = $_POST['password'];
 
-	$query = "SELECT * FROM `users` WHERE `email` = `$email` and `password` =`$password` LIMIT";
+	$query = "SELECT * FROM `users` WHERE `email` = '$email' and `password` = '$password'";
 	$result = $dbConnection->query($query);
 
-	if ($result->num_rows === 1){
-		$_SESSION['online'] = true;
+	if($result->num_rows > 0){
+		echo "successful";
 		?>
-		<!-- closes php tag and opens script tag for local storage -->
 			<script> 
-				localStorage.setItem('online_status', true) 
-				localStorage.setItem('status', result['is_admin']) 
-			</script>
-		<!-- opens php tag and closed script tag  -->
+				localStorage.setItem('online_status', true)  
+				localStorage.setItem('status', result['is_admin'])  
+			</script> 
 		<?php
 		header("Location: index.php");
-	} 
+	} else echo "Invalid email or password";
 
 	$dbConnection->close()
 ?>
--- signup.php
+++ signup.php
@@ -1,48 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Sign Up</title>
 </head>
-<body>
-	<!-- <?php echo $message ?> -->
-	<form action="./models/signup.php" method="POST">
-	<div>
-		<label for="name">Name</label>
-		<input type="text" name="name" id="name">
-	</div>
-	<div>
-		<label for="email">Email</label>
-		<input type="email" name="email" id="email">
-	</div>
-	<div>
-		<label for="country">Country</label>
-		<input type="text" name="country" id="country">
-	</div>
-	<div>
-		<label for="phone">Phone</label>
-		<input type="number" name="phone" id="phone">
-	</div>
-	<div>
-		<label for="gender">Gender</label>
-		<input type="text" name="gender" id="gender">
-	</div>
-	<div>
-		<label for="dob">Date Of Birth</label>
-		<input type="text" name="dob" id="dob">
-	</div>
-	<div>
-		<label for="password">Password</label>
-		<input type="password" name="password" id="password">
-	</div>
-	<div>
-		<label for="password">Confirm Password</label>
-		<input type="password" name="cpassword" id="cpassword">
-	</div>
 
-	<button type="submit">SIgn up</button>
+<body>
+	<form action="./models/signup.php" method="POST">
+		<div>
+			<label for="name">Name</label>
+			<input type="text" name="name" id="name">
+		</div>
+		<div>
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email">
+		</div>
+		<div>
+			<label for="country">Country</label>
+			<input type="text" name="country" id="country">
+		</div>
+		<div>
+			<label for="phone">Phone</label>
+			<input type="number" name="phone" id="phone">
+		</div>
+		<div>
+			<label for="gender">Gender</label>
+			<input type="text" name="gender" id="gender">
+		</div>
+		<div>
+			<label for="dob">Date Of Birth</label>
+			<input type="text" name="dob" id="dob">
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password">
+		</div>
+		<div>
+			<label for="password">Confirm Password</label>
+			<input type="password" name="cpassword" id="cpassword">
+		</div>
+
+		<button type="submit">Sign up</button>
 	</form>
 </body>
+
 </html>
